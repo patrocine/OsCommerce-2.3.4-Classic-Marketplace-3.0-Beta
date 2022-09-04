45186befe0d4cfb477b365187e5839619669a3de
patrocine <110309388+patrocine@users.noreply.github.com>
2022-09-04 03:21:55 +0100

    Add files via upload

---
+++ admin/includes/languages/idioma.php
@@ -0,0 +1,16 @@
+// BOE Access with Level Account (v. 2.2a) for the Admin Area of osCommerce (MS2) 1 of 1
+// comment out or remove below lines to disable this contribution
+// header text in includes/header.php
+define('HEADER_TITLE_ACCOUNT', 'Mi Cuenta');
+define('HEADER_TITLE_LOGOFF_ADMIN', 'Salir');
+
+// Admin Account
+define('BOX_HEADING_MY_ACCOUNT', 'Mi cuenta');
+
+// configuration box text in includes/boxes/administrator.php
+define('BOX_HEADING_ADMINISTRATOR', 'Administrador');
+define('BOX_ADMINISTRATOR_MEMBERS', 'Grupos');
+define('BOX_ADMINISTRATOR_MEMBER', 'Miembros');
+define('BOX_ADMINISTRATOR_BOXES', 'Acceso a ficheros');
+define('TEXT_DISPLAY_NUMBER_OF_SALES', 'Numero de Paginas:');
+
