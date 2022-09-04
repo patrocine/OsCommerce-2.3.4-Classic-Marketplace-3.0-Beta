8fcb7e7d55cc08de22c122394ff12b692a847678
patrocine <110309388+patrocine@users.noreply.github.com>
2022-09-04 13:22:16 +0100

    Add files via upload

---
+++ includes/languages/shopping_cart.php
@@ -0,0 +1,27 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2007 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+define('NAVBAR_TITLE', 'Contenido de la cesta');
+define('HEADING_TITLE', 'Que hay en mi cesta?');
+define('TABLE_HEADING_PRODUCTS', 'Producto(s)');
+define('TEXT_CART_EMPTY', 'Tu cesta de la compra esta vacia!');
+define('SUB_TITLE_SUB_TOTAL', 'Subtotal:');
+define('SUB_TITLE_TOTAL', 'Total:');
+define('TEXT_OR', 'o ');
+define('TEXT_REMOVE', 'quitar');
+
+
+define('OUT_OF_STOCK_CANT_CHECKOUT', 'Los productos marcados con ' . STOCK_MARK_PRODUCT_OUT_OF_STOCK . ' no estan disponibles en la cantidad que requiere.<br>Modifique la cantidad de productos marcados con ' . STOCK_MARK_PRODUCT_OUT_OF_STOCK . ', Gracias');
+define('OUT_OF_STOCK_CAN_CHECKOUT', 'Los productos marcados con ' . STOCK_MARK_PRODUCT_OUT_OF_STOCK . ' no estan disponibles en la cantidad que requiere.<br>De todas formas, puede comprar los que hay disponibles y el resto se lo enviamos mas tarde o esperar a que la cantidad requerida este disponible.');
+
+define('TEXT_ALTERNATIVE_CHECKOUT_METHODS', '- O -');
+?>
---
+++ includes/languages/specials.php
@@ -0,0 +1,15 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2011 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+define('NAVBAR_TITLE', 'Ofertas');
+define('HEADING_TITLE', 'Productos en oferta');
+?>
